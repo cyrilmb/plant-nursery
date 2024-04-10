9d04afa11b936f78c8e51b84d907a81b7d068a69
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-04-09 21:14:16 -0500

    created a couple fake products, might need more

--- lib/data.ts
+++ lib/data.ts
@@ -1,1 +1,30 @@
-const 
+const data = {
+    products: [
+        {
+        name: 'Queen of the Prairie',
+        slug: 'queen-of-the-prairie',
+        image: '/images/queen-of-the-prairie.jpg',
+        price: 5,
+        sci_name: 'Filipendula rubra',
+        description: 'Queen of the Prairie heralds fragile sprays of tiny pink buds in early summer.  These pearlescent clusters burgeon out and burst open from bottom to top in an abundance of little flowers attended by a fanfare of filaments.  Floating above the surrounding plants, the cotton candy-colored plumes look exactly like the favorite carnival food.  Color saturation often increases as the flowers mature, ranging from the faintest of pinks to rich, regal magentas.  These blooms ascend from lovely, deeply lobed foliage that provide an attractive backdrop after the showstopping flowers are spent.  Deadheading does not signal for more flower production, so enjoy them while they last!  The stems can sometimes be a ruddy red.',
+        sun_exposure: 'Full',
+        soil_moisture: 'Wet, Medium-Wet',
+        height: '5 feet',
+        bloom_time: 'June, July, August',
+        bloom_color: 'Pink'
+    },
+    {
+        name: 'Dense Blazing Star',
+        slug: 'dense-blazing-star',
+        image: '/images/dense-blazing-star.jpg',
+        price: 5,
+        sci_name: 'Liatris spicata',
+        description: 'The most moisture-tolerant of Liatris species, this Blazing Star does equally well in sunny, well-drained garden sites. For about three weeks in mid- to late summer, it sports purple wands of stemless, crowded flowers, facing all directions and blossoming from the top of the stem down. Marsh Blazing Star is its other common name.  The alternating leaves of the plant are grass-like, up to 10” long and dense at the base, becoming smaller as they ascend the stem.',
+        sun_exposure: 'Full, Partial',
+        soil_moisture: 'Wet, Medium-Wet, Medium',
+        height: '5 feet',
+        bloom_time: 'July, August, September',
+        bloom_color: 'Purple'
+    }
+]
+}
