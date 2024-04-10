2ba00c23157f3ab20f7b5b12e0dd5946da961f21
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-04-09 20:59:14 -0500

    product card and price, need filler data to check

--- components/products/ProductItem.tsx
+++ components/products/ProductItem.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
+import { Product } from '@/lib/models/ProductModel'
+import Link from 'next/link'
+import Image from 'next/image'
 
-export default function ProductItem() {
-  return <div></div>
+export default function ProductItem({ product }: { product: Product }) {
+  return (
+    <div className="card bg-base-300 shadow-lg mb-4">
+      <figure>
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={300}
+            height={300}
+            className="object-cover h-64 w-full"
+          />
+        </Link>
+      </figure>
+      <div className="card-body">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="card-title font-normal">{product.name}</h2>
+        </Link>
+        <div className="card-actions flex items-center justify-between">
+          <span className="text-2xl">${product.price}</span>
+        </div>
+      </div>
+    </div>
+  )
 }
