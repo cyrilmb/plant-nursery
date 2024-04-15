467b1fd755c2d6117d91f3d8d91ab294bbd0ffc3
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-04-15 07:23:06 -0500

    made table for checkout cart

--- app/(front)/cart/CartDetails.tsx
+++ app/(front)/cart/CartDetails.tsx
@@ -2,7 +2,8 @@
 
 import useCartService from '@/lib/hooks/useCartStore'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 export default function CartDetails() {
@@ -26,7 +27,57 @@ export default function CartDetails() {
           Cart is empty <Link href="/">Go Shopping!</Link>
         </div>
       ) : (
-        <div className="grid md:grid-cols-4 md:gap-5"></div>
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map((item) => (
+                  <tr key={item.slug}>
+                    <td>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        ></Image>
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </td>
+                    <td>
+                      <button
+                        className="btn"
+                        type="button"
+                        onClick={() => decrease(item)}
+                      >
+                        -
+                      </button>
+                      <span>{item.qty}</span>
+                      <button
+                        className="btn"
+                        type="button"
+                        onClick={() => decrease(item)}
+                      >
+                        +
+                      </button>
+                    </td>
+                    <td>${item.price}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       )}
     </>
   )
