920dfaff21ad240a9aee24a278e4737de9278515
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-07 17:47:49 -0500

    created model of product for db

--- lib/models/ProductModel.ts
+++ lib/models/ProductModel.ts
@@ -1,3 +1,32 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema(
+    {
+        name : {type: String, required: true},
+        slug : {type: String, required: true, unique: true},
+        image : {type: String, required: true},
+        price : {type: Number, required: true},
+        sciName : {type: String, required: true},
+        description : {type: String, required: true},
+        sunExposure : {type: String, required: true},
+        soilMoisture : {type: String, required: true},
+        height : {type: String, required: true},
+        bloomTime : {type: String, required: true},
+        bloomColor : {type: String, required: true},
+        countInStock : {type: Number, required: true, default: 0},
+        banner: String
+}, 
+{
+    //records time of creation and last update recorded
+    timestamps: true
+}
+)
+
+const ProductModel = 
+    mongoose.models.Product || mongoose.model('Product', productSchema)
+
+    export default ProductModel
+
 export type Product = {
     _id?: string
     name: string
