9dbf429fcb657da20cea6f6c78bea3a8bc58b639
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-10 21:14:16 -0500

    using credentialsprovider to check email and hashed pw

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,16 +1,34 @@
-import { CredentialsProvider } from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
+import dbConnect from "./dbConnect";
+import UserModel from "./models/UserModel";
+import bcrypt from 'bcryptjs'
 
 export const config = {
     providers: [
-        CredentialProvider({
-            credentials: {
-                email: {
-                    type: 'email',
-                },
-                async authorize(credentials) {
-                    
-                }
+        CredentialsProvider({
+          credentials: {
+            email: {
+              type: 'email',
+            },
+            password: { type: 'password' },
+          },
+          async authorize(credentials) {
+            await dbConnect()
+            if (credentials == null) return null
+    
+            const user = await UserModel.findOne({ email: credentials.email })
+    
+            if (user) {
+              const isMatch = await bcrypt.compare(
+                credentials.password as string,
+                user.password
+              )
+              if (isMatch) {
+                return user
+              }
             }
-        })
-    ]
+            return null
+          },
+        }),
+      ],
 }
