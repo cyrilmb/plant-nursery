53984ce236db4834c277f923b5d740cdde4391b0
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-13 14:53:56 -0500

    user auth as middleware and allowing some routes

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+//Pass all URLs through middleware to check user auth
+export { auth as middleware } from './lib/auth'
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
