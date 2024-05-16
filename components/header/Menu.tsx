4ec78e4918659d708f2fe8c1e468ab07f8f4ae80
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-16 12:38:52 -0500

    signout button, user name if signed in

--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import useCartService from '@/lib/hooks/useCartStore'
-import { signOut, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
@@ -35,9 +35,51 @@ const Menu = () => {
             )}
           </Link>
         </li>
-        <button className="btn btn-ghost rounded-btn" type="button">
-          Sign In
-        </button>
+        {session && session.user ? (
+          <>
+            <li>
+              <div className="dropdown dropdown-bottom dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost rounded-btn">
+                  {session.user.name}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu dropdown-content z-[1] p-2 shadow bg-base-300 rounded-box w-52 "
+                >
+                  <li>
+                    <button type="button" onClick={signoutHandler}>
+                      Sign out
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </>
+        ) : (
+          <li>
+            <button
+              className="btn btn-ghost rounded-btn"
+              type="button"
+              onClick={() => signIn()}
+            >
+              Sign in
+            </button>
+          </li>
+        )}
       </ul>
     </div>
   )
