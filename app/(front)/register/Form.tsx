7fc3f93f24352c0f0077040383d18407017edf06
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-16 14:36:55 -0500

    better error message for existing email

--- app/(front)/register/Form.tsx
+++ app/(front)/register/Form.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useSession } from 'next-auth/react'
+import Link from 'next/link'
 import { useSearchParams, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
@@ -65,28 +66,110 @@ const Form = () => {
         throw new Error(data.message)
       }
     } catch (err: any) {
-      toast.error(err.message || 'message')
+      const error =
+        err.message && err.message.indexOf('E11000') === 0
+          ? 'An accout with this email already exists'
+          : err.message
+      toast.error(error || 'message')
     }
   }
   return (
     <div className="max-w-sm mx-auto card bg-base-300 my-4">
       <div className="card-body">
         <h1 className="card-title">Register</h1>
-        <div className="my-2">
-          <label htmlFor="name" className="label">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            {...register('name', {
-              required: 'Name is required',
-            })}
-            className="input input-bordered w-full max-w-sm"
-          />
-          {errors.name?.message && (
-            <div className="text-error">{errors.name.message}</div>
-          )}
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <div className="my-2">
+            <label htmlFor="name" className="label">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              {...register('name', {
+                required: 'Name is required',
+              })}
+              className="input input-bordered w-full max-w-sm"
+            />
+            {errors.name?.message && (
+              <div className="text-error">{errors.name.message}</div>
+            )}
+          </div>
+          <div className="my-2">
+            <label className="label" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                  message: 'Email is invalid',
+                },
+              })}
+              className="input input-bordered w-full max-w-sm"
+            />
+            {errors.email?.message && (
+              <div className="text-error"> {errors.email.message}</div>
+            )}
+          </div>
+          <div className="my-2">
+            <label className="label" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              {...register('password', {
+                required: 'Password is required',
+              })}
+              className="input input-bordered w-full max-w-sm"
+            />
+            {errors.password?.message && (
+              <div className="text-error">{errors.password.message}</div>
+            )}
+          </div>
+          <div className="my-2">
+            <label className="label" htmlFor="confirmPassword">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              {...register('confirmPassword', {
+                required: 'Confirm Password is required',
+                validate: (value) => {
+                  const { password } = getValues()
+                  return password === value || 'Passwords should match!'
+                },
+              })}
+              className="input input-bordered w-full max-w-sm"
+            />
+            {errors.confirmPassword?.message && (
+              <div className="text-error">{errors.confirmPassword.message}</div>
+            )}
+          </div>
+          <div className="my-2">
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="btn btn-primary w-full"
+            >
+              {isSubmitting && (
+                <span className="loading loading-spinner"></span>
+              )}
+              Register
+            </button>
+          </div>
+        </form>
+
+        <div className="divider"> </div>
+        <div>
+          Already have an account?{' '}
+          <Link className="link" href={`/signin?callbackUrl=${callbackUrl}`}>
+            Login
+          </Link>
         </div>
       </div>
     </div>
