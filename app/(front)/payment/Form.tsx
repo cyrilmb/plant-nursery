51446b488ca1635d2c9b4af71673426e703b0b48
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-20 10:15:01 -0500

    payment form working

---
+++ app/(front)/payment/Form.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import CheckoutSteps from '@/components/CheckoutSteps'
+import useCartService from '@/lib/hooks/useCartStore'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+const Form = () => {
+  const router = useRouter()
+  const { savePaymentMethod, paymentMethod, shippingAddress } = useCartService()
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('')
+  const handleSubmit = async (e: React.FormEvent) => {
+    //prevent page refresh
+    e.preventDefault()
+    savePaymentMethod(selectedPaymentMethod)
+    router.push('/place-order')
+  }
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      return router.push('/shipping')
+    }
+    setSelectedPaymentMethod(paymentMethod || 'Paypal')
+  }, [paymentMethod, router, shippingAddress.address])
+
+  return (
+    <div>
+      <CheckoutSteps current={2} />
+      <div className="max-w-sm mx-auto card bg-base-300 my-4">
+        <div className="card-body">
+          <h1 className="card-title">Payment Method</h1>
+          <form onSubmit={handleSubmit}>
+            {['PayPal', 'Stripe', 'CashOnDelivery'].map((payment) => (
+              <div key={payment}>
+                <label className="label cursor-pointer">
+                  <span className="label-text">{payment}</span>
+                  <input
+                    type="radio"
+                    name="paymentMethod"
+                    className="radio"
+                    value={payment}
+                    checked={selectedPaymentMethod === payment}
+                    onChange={() => setSelectedPaymentMethod(payment)}
+                  />
+                </label>
+              </div>
+            ))}
+            <div className="my-2">
+              <button type="submit" className="btn btn-primary w-full">
+                Next
+              </button>
+            </div>
+            <div className="my-2">
+              <button
+                type="button"
+                className="btn w-full my-2"
+                onClick={() => router.back()}
+              >
+                Back
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Form
