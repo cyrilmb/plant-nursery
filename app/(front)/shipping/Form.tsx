82f4d65a9b79ecfd363951d04368f42eac4b6d2d
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-20 09:42:45 -0500

    form for shipping info, need page.tsx for it

--- app/(front)/shipping/Form.tsx
+++ app/(front)/shipping/Form.tsx
@@ -1,1 +1,100 @@
-const Form = ()
+import useCartService from '@/lib/hooks/useCartStore'
+import { ShippingAddress } from '@/lib/models/OrderModel'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import { SubmitHandler, useForm, ValidationRule } from 'react-hook-form'
+import CheckoutSteps from '@/components/CheckoutSteps'
+
+const Form = () => {
+  const router = useRouter()
+  const { saveShippingAddress, shippingAddress } = useCartService()
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<ShippingAddress>({
+    defaultValues: {
+      fullName: '',
+      address: '',
+      city: '',
+      postalCode: '',
+      country: '',
+    },
+  })
+
+  useEffect(() => {
+    setValue('fullName', shippingAddress.fullName)
+    setValue('address', shippingAddress.address)
+    setValue('city', shippingAddress.city)
+    setValue('postalCode', shippingAddress.postalCode)
+    setValue('country', shippingAddress.country)
+  }, [setValue, shippingAddress])
+
+  const formSubmit: SubmitHandler<ShippingAddress> = async (form) => {
+    saveShippingAddress(form)
+    router.push('/payment')
+  }
+
+  const FormInput = ({
+    id,
+    name,
+    required,
+    pattern,
+  }: {
+    id: keyof ShippingAddress
+    name: string
+    required?: boolean
+    pattern?: ValidationRule<RegExp>
+  }) => (
+    <div className="mb-2">
+      <label htmlFor={id} className="label">
+        {name}
+      </label>
+      <input
+        type="text"
+        id={id}
+        {...register(id, {
+          required: required && `${name} is required`,
+          pattern,
+        })}
+        className="input input-bordered w-full max-w-sm"
+      />
+      {errors[id]?.message && (
+        <div className="text-error">{errors[id]?.message}</div>
+      )}
+    </div>
+  )
+
+  return (
+    <div>
+      <CheckoutSteps current={1} />
+      <div className="max-w-sm mx-auto card bg-base-300 my-4">
+        <div className="card-body">
+          <h1 className="card-title">Shipping Address</h1>
+          <form onSubmit={handleSubmit(formSubmit)}>
+            <FormInput name="Full Name" id="fullName" required />
+            <FormInput name="Address" id="address" required />
+            <FormInput name="City" id="city" required />
+            <FormInput name="Postal Code" id="postalCode" required />
+            <FormInput name="Country" id="country" required />
+            <div className="my-2">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="btn btn-primary w-full"
+              >
+                {isSubmitting && (
+                  <span className="loading loading-spinner"></span>
+                )}
+                Next
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Form
