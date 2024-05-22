c061aa3fc4d968a8260b608076c4ab0aa504a577
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-22 11:15:47 -0500

    setup swr on ClientProvider to manage state/data fetch and cache

--- components/ClientProviders.tsx
+++ components/ClientProviders.tsx
@@ -1,7 +1,6 @@
 'use client'
-import { Toaster } from 'react-hot-toast'
-
-//show toast messages in app
+import toast, { Toaster } from 'react-hot-toast'
+import { SWRConfig } from 'swr'
 
 export default function ClientProviders({
   children,
@@ -9,9 +8,24 @@ export default function ClientProviders({
   children: React.ReactNode
 }) {
   return (
-    <>
+    //SWR is alternative to react-query, data fetching, caching, state management
+    <SWRConfig
+      value={{
+        onError: (error, key) => {
+          toast.error(error.message)
+        },
+        fetcher: async (resource, init) => {
+          const res = await fetch(resource, init)
+          if (!res.ok) {
+            throw new Error('An error occured while fetching data.')
+          }
+          return res.json()
+        },
+      }}
+    >
+      {/* show toast messages in app */}
       <Toaster />
       {children}
-    </>
+    </SWRConfig>
   )
 }
