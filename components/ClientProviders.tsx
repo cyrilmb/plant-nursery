6d923a4fdb0133441d909afe01fe8a2bdc2713ee
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-22 11:19:49 -0500

    update cartstore in event of multiple tabs being used so they are all in sync

--- components/ClientProviders.tsx
+++ components/ClientProviders.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { cartStore } from '@/lib/hooks/useCartStore'
+import { useEffect } from 'react'
 import toast, { Toaster } from 'react-hot-toast'
 import { SWRConfig } from 'swr'
 
@@ -7,6 +9,20 @@ export default function ClientProviders({
 }: {
   children: React.ReactNode
 }) {
+  //reload data in cartstore when using multiple browser tabs, so all tabs are in sync
+  const updateStore = () => {
+    cartStore.persist.rehydrate()
+  }
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', updateStore)
+    window.addEventListener('focus', updateStore)
+    return () => {
+      document.removeEventListener('visibilitychange', updateStore)
+      window.removeEventListener('focus', updateStore)
+    }
+  }, [])
+
   return (
     //SWR is alternative to react-query, data fetching, caching, state management
     <SWRConfig
