d542eec1433701c582186680039360f9656ff5f6
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-23 20:03:20 -0500

    created checkout form

--- app/(front)/place-order/Form.tsx
+++ app/(front)/place-order/Form.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import CheckoutSteps from '@/components/CheckoutSteps'
 import useCartService from '@/lib/hooks/useCartStore'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import useSWRMutation from 'swr/mutation'
+import Image from 'next/image'
 
 const Form = () => {
   const router = useRouter()
@@ -47,6 +51,131 @@ const Form = () => {
         toast.error(data.message)
       }
     }
+  )
+  useEffect(() => {
+    if (!paymentMethod) {
+      return router.push('/payment')
+    }
+    if (items.length === 0) {
+      return router.push('/')
+    }
+  }, [paymentMethod, router, items.length])
+
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return <></>
+
+  return (
+    <div>
+      <CheckoutSteps current={4} />
+      <div className="grid md:grid-cols-4 md:gap-5 my-4">
+        <div className="overflow-x-auto md:col-span-3">
+          <div className="card bg-base-300">
+            <div className="card-body">
+              <h2 className="card-title">Shipping Address</h2>
+              <p>{shippingAddress.fullName}</p>
+              <p>
+                {shippingAddress.address}, {shippingAddress.city},{' '}
+                {shippingAddress.postalCode}, {shippingAddress.country}{' '}
+              </p>
+              <div>
+                <Link className="btn" href="/shipping">
+                  Edit
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="card bg-base-300 mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Payment Method</h2>
+              <p>{paymentMethod}</p>
+              <div>
+                <Link className="btn" href="/payment">
+                  Edit
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="card bg-base-300 mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Items</h2>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Item</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {items.map((item) => (
+                    <tr key={item.slug}>
+                      <td>
+                        <Link
+                          href={`/product/${item.slug}`}
+                          className="felx items-center"
+                        >
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          ></Image>
+                          <span className="px-2">{item.name}</span>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="card bg-base-300">
+            <div className="card-body">
+              <h2 className="card-title">Order Summary</h2>
+              <ul className="space-y-3">
+                <li>
+                  <div className="flex justify-between">
+                    <div>Items</div>
+                    <div>${itemsPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex justify-between">
+                    <div>Tax</div>
+                    <div>${taxPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex justify-between">
+                    <div>Total</div>
+                    <div>${totalPrice}</div>
+                  </div>
+                </li>
+                <li>
+                  <button
+                    onClick={() => placeOrder()}
+                    disabled={isPlacing}
+                    className="btn btn-primary w-full"
+                  >
+                    {isPlacing && (
+                      <span className="loading loading-spinner">
+                        Place Order
+                      </span>
+                    )}
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
