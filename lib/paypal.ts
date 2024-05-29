bc438f2e4dd9e9bd8ab3f11b42ec17e9cfcb332a
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-05-29 09:46:48 -0500

    paypal createOrder and capture payment endpoints. building route for orders by id params

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,10 +1,46 @@
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com'
 
-export const paypal = {}
+export const paypal = {
+    createOrder: async function createOrder(price: number) {
+        const accessToken = await generateAccessToken()
+        const url = `${base}/v2/checkout/orders`
+        const response = await fetch(url, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`
+            },
+            body: JSON.stringify({
+                intent: 'CAPTURE',
+                purchase_units: [
+                    {
+                    amount: {
+                        currency_code: 'USD',
+                        value: price
+                    }
+                }
+                ]
+            })
+        })
+        return handleResponse(response)
+    },
+    capturePayment: async function capturePayment(orderId: string) {
+        const accessToken = await generateAccessToken()
+        const url = `${base}/v2/checkout/orders/${orderId}/capture`
+        const response = await fetch(url, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        return handleResponse(response)
+    }
+}
 
 async function generateAccessToken() {
     const {PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET} = process.env
-    //auth is base64 hashed
+    //auth is hashed ID and SECRET for API request
     const auth = Buffer.from(PAYPAL_CLIENT_ID + ':' + PAYPAL_APP_SECRET).toString('base64')
     const response = await fetch(`${base}/v1/oauth2/token`, {
         method: 'post',
